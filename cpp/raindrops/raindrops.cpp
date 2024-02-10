0593c9b886fc31e7e33897e51f6275234b3d6e8c
Maxine Krebs <krebs.maxine@gmail.com>
2024-02-10 14:01:13 -0500

    solution for "raindrops" exercise

--- cpp/raindrops/raindrops.cpp
+++ cpp/raindrops/raindrops.cpp
@@ -1,5 +1,32 @@
 #include "raindrops.h"
+#include <string>
+#include <sstream>
 
 namespace raindrops {
+
+	std::string convert(int number) {
+		std::ostringstream oss;
+		std::string result;
+
+		if (number % 3 != 0 && number % 5 != 0 && number % 7 != 0) {
+			return std::to_string(number);
+		} else {
+			if (number % 3 == 0) {
+				oss << "Pling";
+			}
+
+			if (number % 5 == 0) {
+				oss << "Plang";
+			}
+
+			if (number % 7 == 0) {
+				oss << "Plong";
+			}
+
+			result = oss.str();
+
+			return result;
+		}
+	}
 
 }  // namespace raindrops
