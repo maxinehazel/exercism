be3703353f8e3a0eedfb763c688df60234ecc6cf
Maxine Krebs <krebs.maxine@gmail.com>
2024-02-20 21:45:43 -0500

    [WIP] add clock rust exercise and start solving it

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,26 @@
+use core::fmt;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        Clock { hours, minutes }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        let minutes = self.minutes + minutes;
+        let hours = self.hours;
+
+        Clock { hours, minutes }
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
