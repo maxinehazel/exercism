bbea7934237f4b2d0f949047a259b667449afae9
Maxine Krebs <krebs.maxine@gmail.com>
2024-02-22 17:54:11 -0500

    solve more test failures

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -8,12 +8,28 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock { hours, minutes }
+        Self::calculate_time_overflow(hours, minutes)
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         let minutes = self.minutes + minutes;
         let hours = self.hours;
+
+        Self::calculate_time_overflow(hours, minutes)
+    }
+
+    fn calculate_time_overflow(hours: i32, minutes: i32) -> Self {
+        let mut minutes = minutes;
+        let mut hours = hours;
+
+        while minutes > 60 {
+            hours += 1;
+            minutes -= 60;
+        }
+
+        while hours >= 24 {
+            hours -= 24;
+        }
 
         Clock { hours, minutes }
     }
